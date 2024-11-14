ad30af17a0a3ebece74faace949d04f080e6d554
Bhavesh5650 <bhaveshkatyare@gmail.com>
2024-11-14 13:20:58 +0530

    Browser

--- app/src/main/java/com/example/browser/view/activity/SplashScreen.kt
+++ app/src/main/java/com/example/browser/view/activity/SplashScreen.kt
@@ -1,28 +1,29 @@
-package com.example.browser
+package com.example.browser.view.activity
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.browser.databinding.ActivityMainBinding
+import com.example.browser.MainActivity
+import com.example.browser.R
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_splash_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+        },2000)
     }
 }
